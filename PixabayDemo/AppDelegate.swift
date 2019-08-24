2cf9863bcc98b124f029b0e13f7f7e951b98fff2
Krzysztof Lech <krzysztof.lech@24abc.pl>
2019-08-24 12:04:56 +0200

    Xcode project added

---
+++ PixabayDemo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  PixabayDemo
+//
+//  Created by Krzysztof Lech on 24/08/2019.
+//  Copyright © 2019 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
